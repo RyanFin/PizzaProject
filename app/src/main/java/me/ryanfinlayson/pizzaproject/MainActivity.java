9cb9292eb067cafe938260b99850d8b9e23def39
RyanFin <21296921@student.uwl.ac.uk>
2016-11-20 19:44:21 +0000

    Drawer Almost Complete
    • More Navigation Drawer

--- app/src/main/java/me/ryanfinlayson/pizzaproject/MainActivity.java
+++ app/src/main/java/me/ryanfinlayson/pizzaproject/MainActivity.java
@@ -3,8 +3,11 @@ package me.ryanfinlayson.pizzaproject;
 import android.app.Activity;
 import android.app.Fragment;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.app.FragmentTransaction;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -20,18 +23,68 @@ public class MainActivity extends Activity {
 
     //create a private variable for the shareActionProvider
     private ShareActionProvider shareActionProvider;
+    //create the DrawerListener
+    private ActionBarDrawerToggle drawerToggle;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open_drawer, R.string.close_drawer){
+            //when drawer is fully open
+            @Override
+            public void onDrawerOpened(View drawerView) {
+               // invalidateOptionsMenu();
+                super.onDrawerOpened(drawerView);
+            }
+
+            //when drawer is fully closed
+            @Override
+            public void onDrawerClosed(View drawerView) {
+               // invalidateOptionsMenu();
+                super.onDrawerClosed(drawerView);
+            }
+        };
+        drawerLayout.addDrawerListener(drawerToggle);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setHomeButtonEnabled(true);
 
         //populate the listView with the array of titles
         titles = getResources().getStringArray(R.array.titles);
         drawerList = (ListView) findViewById(R.id.drawer);
         drawerList.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_activated_1, titles ));
         drawerList.setOnItemClickListener(new DrawerItemClickListener());
+        //if the app hasnt changed configuration display Home by default
+        if(savedInstanceState == null){
+            selectItem(0);
+        }
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        //sync the toggle state after onRestoreInstanceState has occurred
+        drawerToggle.syncState();
+        super.onPostCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        drawerToggle.onConfigurationChanged(newConfig);
+        super.onConfigurationChanged(newConfig);
+    }
+
+    //called once we use InvalidateOptionsMenu() for drawer open and close logic
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        //check if the listView drawerlist is fully open
+        boolean drawerOpen = drawerLayout.isDrawerOpen(drawerList);
+        //set the actionShare in the actionBar to invisible i
+        menu.findItem(R.id.action_share).setVisible(!drawerOpen);
+        return super.onPrepareOptionsMenu(menu);
     }
 
     //drawer onClick method
@@ -69,6 +122,10 @@ public class MainActivity extends Activity {
 
         //set the actionBar title
         setActionBarTitle(position);
+        //create a reference to the drawer
+        DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        //close the ListView in the drawer
+        drawerLayout.closeDrawer(drawerList);
 
     }
 
@@ -117,6 +174,10 @@ public class MainActivity extends Activity {
     //actionBar button logic
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        if(drawerToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+
         switch (item.getItemId()){
             case R.id.action_create_order:
                 //code to run when order button  is pressed
